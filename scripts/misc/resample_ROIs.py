51545d61eac65d0a19557b134f24e2dc4b8a0364
Melissa Thye <melissathye@gmail.com>
2024-06-03 15:20:20 +0100

    add resampling script

---
+++ scripts/misc/resample_ROIs.py
@@ -0,0 +1,99 @@
+"""
+Resample ROIs to match resolution of preprocessed functional data
+
+This will often be the same template given that the lab uses the same template within fMRIPrep, 
+but template option can be passed in the config file and will be used as long as it is saved in the 
+<PROJECT>/data/templates folder.
+
+"""
+import nilearn
+from nilearn import image
+import os
+import os.path as op
+import numpy as np
+import argparse
+import pandas as pd
+import glob
+import shutil
+
+# define first level workflow function
+def resample_roi(projDir, roiDir, template):
+
+    # define roi files in directory provided
+    roi_files = glob.glob(op.join(roiDir, '*.nii*'))
+    
+    # grab template
+    template_file = glob.glob(op.join(projDir, 'data', 'templates', '*{}*').format(template))
+    template_name = template[:6] # take first 6 characters
+    # template_name = template.split('_')[0] # take full template name
+    
+    # load and binarize mni file
+    template_img = image.load_img(template_file)
+    
+    # make template directory for resampled ROI files
+    templateDir = op.join(roiDir, '{}'.format(template_name))
+    os.makedirs(templateDir, exist_ok=True)
+
+    for m, roi in enumerate(roi_files):
+        print('Resampling {} to {}'.format(roi, template))
+        
+        # extract file name
+        roi_name = roi.replace('/','-').split('-')[-1]
+        roi_name = roi_name.split('.nii')[0]
+
+        # load roi mask
+        mask_img = image.load_img(roi)
+        
+        # ensure that mask/ROI is binarized
+        mask_bin = mask_img.get_fdata()
+        mask_bin[mask_bin >= 1] = 1 # for values equal to or greater than 1, make 1 (values less than 1 are already 0)
+        mask_bin = image.new_img_like(mask_img, mask_bin) # create a new image of the same class as the initial image        
+        
+        # resample roi mask to template space
+        mask_bin_resampled = image.resample_to_img(mask_bin, template_img, interpolation='nearest')
+        
+        # save resampled roi file
+        resampled_file = op.join(roiDir, '{}_{}.nii.gz'.format(roi_name, template_name)) # add template name to file       
+        mask_bin_resampled.to_filename(resampled_file)
+        
+        # move resampled roi file to templateDir
+        shutil.move(resampled_file, templateDir)
+    
+    return
+
+# define command line parser function
+def argparser():
+    # create an instance of ArgumentParser
+    parser = argparse.ArgumentParser()
+    # attach argument specifications to the parser
+    parser.add_argument('-p', dest='projDir',
+                        help='Project directory')
+    parser.add_argument('-c', dest='config',
+                        help='Configuration file')                                            
+    return parser
+
+# define main function that parses the config file and runs the functions defined above
+def main(argv=None):
+    # call argparser function that defines command line inputs
+    parser = argparser()
+    args = parser.parse_args(argv)   
+        
+    # print if the project directory is not found
+    if not op.exists(args.projDir):
+        raise IOError('Project directory {} not found.'.format(args.projDir))
+    
+    # print if config file is not found
+    if not op.exists(args.config):
+        raise IOError('Configuration file {} not found. Make sure it is saved in your project directory!'.format(args.config))
+    
+    # read in configuration file and parse inputs
+    config_file=pd.read_csv(args.config, sep='\t', header=None, index_col=0).replace({np.nan: None})
+    roiDir=config_file.loc['resample',1]
+    template=config_file.loc['template',1]
+    
+    # pass inputs defined above to main resampling function
+    resample_roi(args.projDir, roiDir, template)
+   
+# execute code when file is run as script (the conditional statement is TRUE when script is run in python)
+if __name__ == '__main__':
+    main()
---
+++ scripts/misc/run_py-script.sh
@@ -0,0 +1,98 @@
+#!/bin/bash
+
+################################################################################
+# RUN MISC PYTHON SCRIPTS ASSOCIATED WITH FMRI PIPELINE
+#
+# This script runs the python script specified in the command call within a nipype singularity:
+# A config.tsv file must exist in the project directory. This file has the processing options passed to
+# the pipeline. These parameters are likely to vary for each study, so must be specified for each project.
+#
+# The nipype singularity was installed using the following code:
+# 	singularity build /EBC/processing/singularity_images/nipype-1.8.6.simg docker://nipype/nipype:latest
+################################################################################
+
+# usage documentation - shown if no text file is provided or if script is run outside EBC directory
+Usage() {
+    echo
+	echo
+    echo "Usage:"
+    echo "./run_py-script.sh <python script> <configuration file name>"
+    echo
+    echo "Example:"
+    echo "./run_py-script.sh resample_ROIs.py config-pixar_mind-body.tsv"
+    echo
+	echo "the config file name (not path!) should be provided"
+    echo
+	echo
+	echo "This script must be run within the /EBC/ directory on the server due to space requirements."
+	echo "The script will terminiate if run outside of the /EBC/ directory."
+	echo
+    echo "Script created by Melissa Thye"
+    echo
+    exit
+}
+[ "$1" = "" ] | [ "$2" = "" ] && Usage
+
+# if the script is run outside of the EBC directory (e.g., in home directory where space is limited), terminate the script and show usage documentation
+if [[ ! "$PWD" =~ "/EBC/" ]]
+then Usage
+fi
+
+# check that inputs are expected file types
+if [ ! ${pipeline##*.} == "py" ]
+then
+	echo
+	echo "The pipeline script was not found."
+	echo "The script must be submitted with (1) a python script and (2) a configuration file name as in the example below."
+	echo
+	echo "./run_py-script.sh resample_ROIs.py config-pixar_mind-body.tsv"
+	echo
+	
+	# end script and show full usage documentation
+	Usage
+fi
+
+if [ ! ${2##*.} == "tsv" ]
+then
+	echo
+	echo "The configuration file was not found."
+	echo "The script must be submitted with (1) a python script and (2) a configuration file name as in the example below."
+	echo
+	echo "./run_py-script.sh resample_ROIs.py config-pixar_mind-body.tsv"
+	echo
+	
+	# end script and show full usage documentation	
+	Usage
+fi
+
+# define python script and configuration options from files passed in script call
+script=$1
+config=$2
+
+# define directories
+projDir=`cat ../../PATHS.txt`
+singularityDir="${projDir}/singularity_images"
+codeDir="${projDir}/scripts/misc"
+
+# convert the singularity image to a sandbox if it doesn't already exist to avoid having to rebuild on each run
+if [ ! -d ${singularityDir}/nipype_sandbox ]
+then
+	singularity build --sandbox ${singularityDir}/nipype_sandbox ${singularityDir}/nipype_nilearn.simg
+fi
+
+# change the location of the singularity cache ($HOME/.singularity/cache by default, but limited space in this directory)
+export SINGULARITY_TMPDIR=${singularityDir}
+export SINGULARITY_CACHEDIR=${singularityDir}
+unset PYTHONPATH
+
+# display script
+echo
+echo "Running" ${script} "for..."
+echo
+
+# run first-level workflow using script specified in script call
+singularity exec -C -B /EBC:/EBC						\
+${singularityDir}/nipype_sandbox						\
+/neurodocker/startup.sh python ${codeDir}/${script}		\
+-p ${projDir}											\
+-c ${projDir}/${config}
