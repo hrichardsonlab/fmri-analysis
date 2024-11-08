ac8355f932c3249ff14a005c604e74ca90475039
Melissa Thye <melissathye@gmail.com>
2024-11-08 17:17:04 +0000

    update brain mask script to generate native space mask

--- scripts/05.motion_exclusions/concat_brain_masks.py
+++ scripts/05.motion_exclusions/concat_brain_masks.py
@@ -19,40 +19,70 @@ def concat_masks(derivDir, sub, ses):
         
         # define path to inputs (subjects preprocessed functional data)
         subDir = op.join(derivDir, 'sub-{}'.format(sub), 'ses-{}'.format(ses), 'func')
-        concat_img_fname = '{}/sub-{}_ses-{}_space-MNI152NLin2009cAsym_res-2_desc-brain_mask_allruns-BOLDmask.nii.gz'.format(subDir, sub, ses)
+        mni_img_fname = '{}/{}_ses-{}_space-MNI152NLin2009cAsym_res-2_desc-brain_mask_allruns-BOLDmask.nii.gz'.format(subDir, sub, ses)
+        t1w_img_fname = '{}/{}_ses-{}_space-T1w_desc-brain_mask_allruns-BOLDmask.nii.gz'.format(subDir, sub, ses)
+        
     else: # if session was 'no'
         # define path to inputs (subjects preprocessed functional data)
         subDir = op.join(derivDir, 'sub-{}'.format(sub), 'func')
-        concat_img_fname = '{}/sub-{}_MNI152NLin2009cAsym_res-2_desc-brain_mask_allruns-BOLDmask.nii.gz'.format(subDir, sub)
+        mni_img_fname = '{}/sub-{}_space-MNI152NLin2009cAsym_res-2_desc-brain_mask_allruns-BOLDmask.nii.gz'.format(subDir, sub, ses)
+        t1w_img_fname = '{}/sub-{}_space-T1w_desc-brain_mask_allruns-BOLDmask.nii.gz'.format(subDir, sub, ses)
     
     # identify all mask files (there should be 1 per functional run)
-    maskfiles = glob.glob(op.join(subDir, '*MNI152NLin2009cAsym_res-2_desc-brain_mask.nii.gz'))  
+    mni_maskfiles = glob.glob(op.join(subDir, '*MNI152NLin2009cAsym_res-2_desc-brain_mask.nii.gz'))  
+    t1w_maskfiles = glob.glob(op.join(subDir, '*T1w_desc-brain_mask.nii.gz'))  
 
-    # if no mask files were found
-    if len(maskfiles) == 0:
-        print('No brain masks found for sub-{}'.format(sub))
-    # if mask files were found
-    else:
-        basemask = maskfiles[0] # take the first mask file as the base image
-        basemask_img = image.load_img(basemask) # load the base mask image
-        concat_mask_img = basemask_img # create the concatenated mask as a copy of the base mask image
+    # generate MNI mask
+    if len(mni_maskfiles) == 0: # if no mask files were found
+        print('No MNI space brain masks found for sub-{}'.format(sub))
+    
+    else: # if mni mask files were found
+        mni_basemask = mni_maskfiles[0] # take the first mask file as the base image
+        mni_basemask_img = image.load_img(mni_basemask) # load the base mask image
+        mni_mask_img = mni_basemask_img # create the concatenated mask as a copy of the base mask image
         
         # if there are more than 1 mask files
-        if len(maskfiles) > 1:
+        if len(mni_maskfiles) > 1:
             # for each mask file (skipping the base mask, 0)
-            for maskI in range(1, len(maskfiles)):
+            for maskI in range(1, len(mni_maskfiles)):
                 # load the mask file
-                mask_img = image.load_img(maskfiles[maskI])
+                mask_img = image.load_img(mni_maskfiles[maskI])
                 # add the mask file to the concatenated mask
-                concat_mask_img = image.math_img('img1 + img2', img1 = concat_mask_img, img2 = mask_img)
+                mni_mask_img = image.math_img('img1 + img2', img1 = mni_mask_img, img2 = mask_img)
 
         # binarize the concatenated mask file
-        concat_mask_data = concat_mask_img.get_fdata() # get image data (as floating point data)
-        concat_mask_data[concat_mask_data >= 1] = 1 # for values equal to or greater than 1, make 1 (values less than 1 are already 0)
-        concat_mask_img_bin = image.new_img_like(basemask_img, concat_mask_data) # create a new image of the same class as the base mask image
+        mni_mask_data = mni_mask_img.get_fdata() # get image data (as floating point data)
+        mni_mask_data[mni_mask_data >= 1] = 1 # for values equal to or greater than 1, make 1 (values less than 1 are already 0)
+        mni_mask_img_bin = image.new_img_like(mni_basemask_img, mni_mask_data) # create a new image of the same class as the base mask image
         
-        concat_mask_img_bin.to_filename(concat_img_fname)
-        print('concatenated mask saved to: {}'.format(concat_img_fname))
+        mni_mask_img_bin.to_filename(mni_img_fname)
+        print('concatenated mask saved to: {}'.format(mni_img_fname))
+
+    # generate T1w mask
+    if len(t1w_maskfiles) == 0: # if no mask files were found
+        print('No MNI space brain masks found for sub-{}'.format(sub))
+    
+    else: # if mni mask files were found
+        t1w_basemask = t1w_maskfiles[0] # take the first mask file as the base image
+        t1w_basemask_img = image.load_img(t1w_basemask) # load the base mask image
+        t1w_mask_img = t1w_basemask_img # create the concatenated mask as a copy of the base mask image
+        
+        # if there are more than 1 mask files
+        if len(t1w_maskfiles) > 1:
+            # for each mask file (skipping the base mask, 0)
+            for maskI in range(1, len(t1w_maskfiles)):
+                # load the mask file
+                mask_img = image.load_img(t1w_maskfiles[maskI])
+                # add the mask file to the concatenated mask
+                t1w_mask_img = image.math_img('img1 + img2', img1 = t1w_mask_img, img2 = mask_img)
+
+        # binarize the concatenated mask file
+        t1w_mask_data = t1w_mask_img.get_fdata() # get image data (as floating point data)
+        t1w_mask_data[t1w_mask_data >= 1] = 1 # for values equal to or greater than 1, make 1 (values less than 1 are already 0)
+        t1w_mask_img_bin = image.new_img_like(t1w_basemask_img, t1w_mask_data) # create a new image of the same class as the base mask image
+        
+        t1w_mask_img_bin.to_filename(t1w_img_fname)
+        print('concatenated mask saved to: {}'.format(t1w_img_fname))
 
 # define command line parser function
 def argparser():
