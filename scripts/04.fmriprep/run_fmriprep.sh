6032876a0fcc40a8ebe461dd031fe58ef924e887
Melissa Thye <melissathye@gmail.com>
2025-11-18 10:32:43 +0000

    update fmriprep script to require config file

--- scripts/04.fmriprep/run_fmriprep.sh
+++ scripts/04.fmriprep/run_fmriprep.sh
@@ -13,10 +13,10 @@ Usage() {
 	echo
 	echo
 	echo "Usage:"
-	echo "./run_fmriprep.sh <list of subjects>"
+	echo "./run_fmriprep.sh <config file> <list of subjects>"
 	echo
 	echo "Example:"
-	echo "./run_fmriprep.sh TEBC-5y_subjs.txt"
+	echo "./run_fmriprep.sh config-pixar_mind-body.tsv TEBC-5y_subjs.txt"
 	echo
 	echo "TEBC-5y_subjs.txt is a file containing the participants to run fMRIPrep on:"
 	echo "8010"
@@ -27,7 +27,7 @@ Usage() {
 	echo "This script must be run within the /data/EBC/ directory on the server due to space requirements."
 	echo "The script will terminiate if run outside of the /data/EBC/ directory."
 	echo
-	echo "Script created by Manuel Blesa & Melissa Thye"
+	echo "Script created by Melissa Thye"
 	echo
 	exit
 }
@@ -38,35 +38,56 @@ if [[ ! "$PWD" =~ "/EBC/" ]]
 then Usage
 fi
 
+if [ ! ${1##*.} == "tsv" ]
+then
+	echo
+	echo "The configuration file was not found."
+	echo "The script must be submitted with (1) a configuration file name and (2) a subject-run list as in the example below."
+	echo
+	echo "./run_fmriprep.sh config-pixar_mind-body.tsv TEBC-5y_subjs.txt"
+	echo	
+	# end script and show full usage documentation	
+	Usage
+fi
+
+if [ ! ${2##*.} == "txt" ]
+then
+	echo
+	echo "The list of participants was not found."
+	echo "The script must be submitted with (1) a configuration file name and (2) a subject-run list as in the example below."
+	echo
+	echo "./run_fmriprep.sh config-pixar_mind-body.tsv TEBC-5y_subjs.txt"
+	echo	
+	# end script and show full usage documentation	
+	Usage
+fi
+
 # define directories
 projDir=`cat ../../PATHS.txt`
 singularityDir="${projDir}/singularity_images"
+
+# define config file and subjects from files passed in script call
+config=${projDir}/$1
+subjs=$(cat $2 | awk '{print $1}')
+
+# define data directories depending on study information
+sharedDir=$(awk -F'\t' '$1=="sharedDir"{print $2}' "$config")
+bidsDir=$(awk -F'\t' '$1=="bidsDir"{print $2}' "$config")
+derivDir=$(awk -F'\t' '$1=="derivDir"{print $2}' "$config")
+
+# extract preprocessing relevant values from config file
+multiecho=$(awk -F'\t' '$1=="multiecho"{print $2}' "$config")
+
+# strip extra formatting if present
+sharedDir="${sharedDir%$'\r'}"
+bidsDir="${bidsDir%$'\r'}"
+derivDir="${derivDir%$'\r'}"
+multiecho="${multiecho%$'\r'}"
 
 # convert the singularity image to a sandbox if it doesn't already exist to avoid having to rebuild on each run
 if [ ! -d ${singularityDir}/fmriprep_sandbox ]
 then
 	apptainer build --sandbox ${singularityDir}/fmriprep_sandbox ${singularityDir}/fmriprep-24.0.0.simg
-fi
-
-# define subjects from text document
-subjs=$(cat $1 | awk '{print $1}') 
-
-# extract sample from list of subjects filename (i.e., are these pilot or HV subjs)
-sample=` basename $1 | cut -d '-' -f 3 | cut -d '.' -f 1 `
-cohort=` basename $1 | cut -d '_' -f 1 `
-
-# define data directories depending on sample information
-if [[ ${sample} == 'pilot' ]]
-then
-	bidsDir="/data/EBC/preprocessedData/${cohort}/BIDs_data/pilot"
-	derivDir="/data/EBC/preprocessedData/${cohort}/derivatives/pilot"
-elif [[ ${sample} == 'HV' ]]
-then
-	bidsDir="/data/EBC/preprocessedData/${cohort}-adultpilot/BIDs_data"
-	derivDir="/data/EBC/preprocessedData/${cohort}-adultpilot/derivatives"
-else
-	bidsDir="/data/EBC/preprocessedData/${cohort}/BIDs_data"
-	derivDir="/data/EBC/preprocessedData/${cohort}/derivatives"
 fi
 
 # export freesurfer license file location
@@ -88,36 +109,66 @@ echo "${subjs}"
 # iterate for all subjects in the text file
 while read p
 do
-	NAME=` basename ${p} | awk -F- '{print $NF}' `	# subj number from folder name
+	sub=` basename ${p} `
 
 	echo
-	echo "Running fMRIprep for sub-${NAME}"
+	echo "Running fMRIprep for sub-${sub}"
 	echo
-
-	# run singularity
-	apptainer run -C -B /data/EBC:/data/EBC,${singularityDir}:/opt/templateflow	\
-	${singularityDir}/fmriprep_sandbox  								\
-	${bidsDir} ${derivDir}												\
-	participant															\
-	--participant-label ${NAME}											\
-	--skip_bids_validation												\
-	--nthreads 16														\
-	--omp-nthreads 16													\
-	--ignore slicetiming												\
-	--fd-spike-threshold 1												\
-	--dvars-spike-threshold 1.5											\
-	--output-space MNI152NLin2009cAsym:res-2 T1w						\
-	--return-all-components												\
-	--derivatives ${derivDir}											\
-	--stop-on-first-crash												\
-	-w ${singularityDir}												\
-	--fs-license-file ${license}  > ${derivDir}/sub-${NAME}/log_fmriprep_sub-${NAME}.txt
-
+	
+	# run fmriprep depending on whether data are multi-echo
+	if [[ "${multiecho}" == "yes" ]]
+	then
+		echo "Data were aquired with a multi-echo sequence. Running multi-echo fMRIPrep command..."
+		
+		# run singularity
+		apptainer run -C -B /data/EBC:/data/EBC,${singularityDir}:/opt/templateflow				\
+		${singularityDir}/fmriprep_sandbox														\
+		${bidsDir} ${derivDir}																	\
+		participant																				\
+		--participant-label ${sub}																\
+		--skip_bids_validation																	\
+		--nthreads 16																			\
+		--omp-nthreads 16																		\
+		--ignore slicetiming																	\
+		--fd-spike-threshold 1																	\
+		--dvars-spike-threshold 1.5																\
+		--me-t2s-fit-method curvefit															\
+		--me-output-echos																		\
+		--output-space MNI152NLin2009cAsym:res-2 T1w											\
+		--return-all-components																	\
+		--derivatives ${derivDir}																\
+		--stop-on-first-crash																	\
+		-w ${singularityDir}																	\
+		--fs-license-file ${license}  > ${derivDir}/sub-${sub}/log_fmriprep_sub-${sub}.txt
+	# if data were not acquired with multiecho sequence
+	else
+		echo "Data were not acquired with a multi-echo sequence. Running default fMRIPrep command..."
+		
+		# run singularity
+		apptainer run -C -B /data/EBC:/data/EBC,${singularityDir}:/opt/templateflow				\
+		${singularityDir}/fmriprep_sandbox														\
+		${bidsDir} ${derivDir}																	\
+		participant																				\
+		--participant-label ${sub}																\
+		--skip_bids_validation																	\
+		--nthreads 16																			\
+		--omp-nthreads 16																		\
+		--ignore slicetiming																	\
+		--fd-spike-threshold 1																	\
+		--dvars-spike-threshold 1.5																\
+		--output-space MNI152NLin2009cAsym:res-2 T1w											\
+		--return-all-components																	\
+		--derivatives ${derivDir}																\
+		--stop-on-first-crash																	\
+		-w ${singularityDir}																	\
+		--fs-license-file ${license}  > ${derivDir}/sub-${sub}/log_fmriprep_sub-${sub}.txt
+	fi
+	
 	# move subject report and freesurfer output files to appropriate directories
 	mv ${derivDir}/*dseg.tsv ${derivDir}/sourcedata/freesurfer
-	mv ${derivDir}/sub-${NAME}.html ${derivDir}/sub-${NAME}
+	mv ${derivDir}/sub-${sub}.html ${derivDir}/sub-${sub}
 	
 	# give other users permissions to created files
-	#chmod -R a+wrx ${derivDir}/sub-${NAME}
+	#chmod -R a+wrx ${derivDir}/sub-${sub}
 
-done <$1
+done <$2
