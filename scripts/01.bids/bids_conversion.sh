9bafd671ada63c21b25c55fabb80c7714f0d8147
Melissa Thye <melissathye@gmail.com>
2026-01-26 14:50:29 +0000

    modify bids conversion script to run singularity

--- scripts/01.bids/bids_conversion.sh
+++ scripts/01.bids/bids_conversion.sh
@@ -2,72 +2,85 @@
 
 ################################################################################
 # CONVERT DICOM DATA TO BIDS FORMAT
+#
+# The dcm2bids singularity was installed using the following code:
+# 	APPTAINER_TMPDIR=/RichardsonLab/processing APPTAINER_CACHEDIR=/RichardsonLab/processing sudo apptainer pull /RichardsonLab/processing/singularity_images/dcm2bids.simg docker://unfmontreal/dcm2bids:3.2.0
+#
 ################################################################################
 
 # usage documentation - shown if no text file is provided
 Usage() {
 	echo
 	echo "Usage:"
-	echo "./bids_conversion.sh <list of subject folders to convert>"
+	echo "./bids_conversion.sh <config file> <list of subject folders to convert>"
 	echo
 	echo "Example:"
-	echo "./bids_conversion.sh TEBC-5y_subjs.txt"
+	echo "./bids_conversion.sh config-AWE1_emo-phys.tsv RLABAWE1_subjs.txt"
 	echo 
-	echo "TEBC-5y_subjs.txt is a file containing the participants to convert"
-	echo "001"
-	echo "002"
+	echo "RLABAWE1_subjs.txt is a file containing the participants to convert"
+	echo "RLAB_AWE1_01"
+	echo "RLAB_AWE1_02"
 	echo "..."
 	echo
 	echo
-	echo "Script created by Manuel Blesa & Melissa Thye"
+	echo "Script created by Melissa Thye"
 	echo
 	exit
 }
-[ "$1" = "" ] && Usage
+[ "$1" = "" ] | [ "$2" = "" ] && Usage
 
-# indicate whether session folders are used (always 'yes' for EBC data)
-sessions='yes'
-
-# extract sample from list of subjects filename (i.e., are these pilot or HV subjs)
-sample=` basename $1 | cut -d '-' -f 3 | cut -d '.' -f 1 `
-cohort=` basename $1 | cut -d '_' -f 1 `
-
-# define location of shared tools (dcm2bids)
-toolDir="/EBC/processing/tools"
-
-# define data directories depending on sample information
-if [[ ${sample} == 'pilot' ]]
-then
-	# define data directories
-	dataDir="/EBC/rawData/${cohort}"
-	bidsDir="/EBC/preprocessedData/${cohort}/BIDs_data/pilot"
-	
-	# define config file to use for bids conversion
-	config=${toolDir}/dcm2bids/${cohort}_config_file-pilot.json
-elif [[ ${sample} == 'HV' ]]
-then
-	# define data directories
-	dataDir="/EBC/rawData/${cohort}-adultpilot"
-	bidsDir="/EBC/preprocessedData/${cohort}-adultpilot/BIDs_data"
-	# define config file to use for bids conversion
-	config=${toolDir}/dcm2bids/${cohort}_config_file.json
-else
-	# define data directories
-	dataDir="/EBC/rawData/${cohort}"
-	bidsDir="/EBC/preprocessedData/${cohort}/BIDs_data"
-	# define config file to use for bids conversion
-	config=${toolDir}/dcm2bids/${cohort}_config_file.json
+# if the script is run outside of the RichardsonLab directory (e.g., in home directory where space is limited), terminate the script and show usage documentation
+if [[ ! "$PWD" =~ "/RichardsonLab/" ]]; 
+then Usage
 fi
+
+if [ ! ${1##*.} == "tsv" ]
+then
+	echo
+	echo "The configuration file was not found."
+	echo "The script must be submitted with (1) a configuration file name and (2) a subject-run list as in the example below."
+	echo
+	echo "./bids_conversion.sh config-AWE1_emo-phys.tsv RLABAWE1_subjs.txt"
+	echo	
+	# end script and show full usage documentation	
+	Usage
+fi
+
+if [ ! ${2##*.} == "txt" ]
+then
+	echo
+	echo "The list of participants was not found."
+	echo "The script must be submitted with (1) a configuration file name and (2) a subject-run list as in the example below."
+	echo
+	echo "./bids_conversion.sh config-AWE1_emo-phys.tsv RLABAWE1_subjs.txt"
+	echo	
+	# end script and show full usage documentation	
+	Usage
+fi
+
+# define directories
+projDir=`cat ../../PATHS.txt`
+singularityDir="${projDir}/singularity_images"
+
+# define config file and subjects from files passed in script call
+config=${projDir}/$1
+subjs=$(cat $2 | awk '{print $1}')
+
+# define data directories depending on study information
+sharedDir=$(awk -F'\t' '$1=="sharedDir"{print $2}' "$config")
+bidsDir=$(awk -F'\t' '$1=="bidsDir"{print $2}' "$config")
+rawDir=$(awk -F'\t' '$1=="rawDir"{print $2}' "$config")
+
+# strip extra formatting if present
+sharedDir="${sharedDir%$'\r'}"
+bidsDir="${bidsDir%$'\r'}"
+rawDir="${rawDir%$'\r'}"
 
 # define temporary directory to transfer raw data to for BIDS conversion 
 tmpDir="${bidsDir}/tmp"
 
-# print confirmation of sample and directory
-echo 'Converting' ${sample} 'data to BIDS format in' ${bidsDir}
-
-# define number of dicoms per functional run for data checking
-pixar_dicoms=214  # 324 in complete run
-sesame_dicoms=260 # 394 in complete run
+# extract study name from bidsDir (used to map to correct dcm2bids config file)
+study=$(basename "${bidsDir}")
 
 # create BIDS directory if it doesn't exist
 if [ ! -d ${bidsDir} ]
@@ -85,126 +98,162 @@ fi
 if [ ! -f ${bidsDir}/dataset_description.json ]
 then
 	echo '{' >> ${bidsDir}/dataset_description.json
-	echo ' "Name": "TheirWorld Edinburgh Birth Cohort (TEBC)",' >> ${bidsDir}/dataset_description.json
+	echo ' "Name": "RichardsonLab AWE Study 1",' >> ${bidsDir}/dataset_description.json
 	echo ' "BIDSVersion": "1.9.0",' >> ${bidsDir}/dataset_description.json
 	echo ' "License": ["This dataset is made available under the Public Domain Dedication and License v1.0, whose full text can be found at http://www.opendatacommons.org/licenses/pddl/1.0/. We hope that all users will follow the ODC Attribution/Share-Alike Community Norms (http://www.opendatacommons.org/norms/odc-by-sa/); in particular, while not legally required, we hope that all users of the data will acknowledge the OpenfMRI project and NSF Grant OCI-1131441 (R. Poldrack, PI) in any publications."],' >> ${bidsDir}/dataset_description.json
-	echo ' "ReferencesAndLinks": ["Boardman JP, Hall J, Thrippleton MJ, Reynolds RM, Bogaert D, Davidson DJ, Schwarze J, Drake AJ, Chandran S, Bastin ME, et al. 2020. Impact of preterm birth on brain development and long-term outcome: Protocol for a cohort study in Scotland. BMJ Open. 10. doi: 10.1136/bmjopen-2019-035854."]' >> ${bidsDir}/dataset_description.json
 	echo '}' >> ${bidsDir}/dataset_description.json
 fi
+
+# change the location of the singularity cache ($HOME/.singularity/cache by default, but limited space in this directory)
+export APPTAINER_TMPDIR=${singularityDir}
+export APPTAINER_CACHEDIR=${singularityDir}
+unset PYTHONPATH
+
+# display subjects
+echo
+echo "Converting data to BIDS format in" ${rawDir} "for..."
+echo "${subjs}"
+echo
+
+# define number of dicoms per functional run for data checking
+awe_dicoms=150 # variable number of volumes in complete runs, so just set this to a reasonable estimate
+tomloc_dicoms=71  # 214 in complete run
+langloc_dicoms=86 # 260 in complete run
+pixar_dicoms=55 # 168 in complete run
+snackattack_dicoms=46 # 140 in complete run
 
 # iterate for all subjects in the text file
 while read p
 do
-	NAME=` basename ${p} | awk -F- '{print $NF}' `	# subj number
+	raw_name=` basename ${p} | awk -F- '{print $NF}' `	# raw subj name
+	bids_name=$(echo "$raw_name" | tr -d '_ ') # bids formatted subj name
 	
 	# convert data if the subject has a raw data folder, trying different naming conventions to check whether the folder exists
-	if [ -d ${dataDir}/"${NAME}" ] || [ -d ${dataDir}/"${cohort^^}-${NAME}" ] || [ -d ${dataDir}/"${cohort^^}P-${NAME}" ] || [ -d ${dataDir}/"${cohort^^}P-TEBC-5YP-${NAME}" ] || [ -d ${dataDir}/"${cohort^^}-AP-${NAME}" ]
+	if [ -d ${rawDir}/${raw_name} ]
 	then
-		
+	
 		# copy rawData to temporary directory
-		echo
-		echo "Copying sub-${NAME} to temporary directory within BIDS_data folder for conversion"
+		echo "Copying ${raw_name} to temporary directory within BIDS folder for conversion"
 		echo
 		
-		cp -r ${dataDir}/*${NAME} ${tmpDir}/${NAME}
+		cp -r ${rawDir}/${raw_name} ${tmpDir}
 		
-		# remove some of the DTI directories
-		rm -r ${tmpDir}/${NAME}/*/*_DTI_AP_*/
+		# remove SBRef files
+		rm -r ${tmpDir}/${tmpDir}/${raw_name}/*/*_SBRef
 		
 		# identify all functional runs
-		pixar_runs=$(ls -d ${tmpDir}/${NAME}/*/*_pixar*/)
-		sesame_runs=$(ls -d ${tmpDir}/${NAME}/*/*_sesame*/)
+		awe_runs=$(ls -d ${tmpDir}/${raw_name}/*/*_awe*/)
+		tomloc_runs=$(ls -d ${tmpDir}/${raw_name}/*/*_tomloc*/)
+		langloc_runs=$(ls -d ${tmpDir}/${raw_name}/*/*_langloc*/)
+		pixar_runs=$(ls -d ${tmpDir}/${raw_name}/*/*_pixar*/)
+		snackattack_runs=$(ls -d ${tmpDir}/${raw_name}/*/*_snack*/)
+		
+		# check awe data
+		for a in ${awe_runs}
+		do
+			echo "Checking awe run" ${a}
+		
+			# check whether run has too few dicoms and if so remove prior to conversion
+			if [[ ! "$(ls ${a} | wc -l)" -ge ${awe_dicoms} ]]
+			then
+				echo
+				echo ${a} "is an incomplete run and will be excluded from BIDS conversion"
+				echo
+				rm -r ${a}
+			fi
+		done
+		
+		# check tomloc data
+		for t in ${tomloc_runs}
+		do
+			echo "Checking tomloc run" ${t}
+		
+			# check whether run has too few dicoms and if so remove prior to conversion
+			if [[ ! "$(ls ${t} | wc -l)" -ge ${tomloc_dicoms} ]]
+			then
+				echo
+				echo ${t} "is an incomplete run and will be excluded from BIDS conversion"
+				echo
+				rm -r ${t}
+			fi
+		done
+		
+		# check langloc data
+		for l in ${langloc_runs}
+		do
+			echo "Checking langloc run" ${l}
+		
+			# check whether run has too few dicoms and if so remove prior to conversion
+			if [[ ! "$(ls ${l} | wc -l)" -ge ${langloc_dicoms} ]]
+			then
+				echo
+				echo ${l} "is an incomplete run and will be excluded from BIDS conversion"
+				echo
+				rm -r ${l}
+			fi
+		done
 		
 		# check pixar data
-		for pr in ${pixar_runs}
+		for p in ${pixar_runs}
 		do
-			echo "Checking pixar run" ${pr}
+			echo "Checking pixar run" ${p}
 		
 			# check whether run has too few dicoms and if so remove prior to conversion
-			if [[ ! "$(ls ${pr} | wc -l)" -ge ${pixar_dicoms} ]]
+			if [[ ! "$(ls ${p} | wc -l)" -ge ${pixar_dicoms} ]]
 			then
 				echo
-				echo ${pr} "is an incomplete run and will be excluded from BIDS conversion"
+				echo ${p} "is an incomplete run and will be excluded from BIDS conversion"
 				echo
-				rm -r ${pr}
+				rm -r ${p}
 			fi
 		done
 		
-		# check sesame data
-		for sr in ${sesame_runs}
+		# check snackattack data
+		for s in ${snackattack_runs}
 		do
-			echo "Checking sesame run" ${sr}
+			echo "Checking snackattack run" ${s}
 		
 			# check whether run has too few dicoms and if so remove prior to conversion
-			if [[ ! "$(ls ${sr} | wc -l)" -ge ${sesame_dicoms} ]]
+			if [[ ! "$(ls ${s} | wc -l)" -ge ${snackattack_dicoms} ]]
 			then
 				echo
-				echo ${sr} "is an incomplete run and will be excluded from BIDS conversion"
+				echo ${s} "is an incomplete run and will be excluded from BIDS conversion"
 				echo
-				rm -r ${sr}
+				rm -r ${s}
 			fi
 		done
-
+		
 		# BIDS conversion
 		echo
-		echo "Converting sub-${NAME} to BIDS"
+		echo "Converting sub-${bids_name} to BIDS"
 		echo
 		
-		# activate conda environment via bash
-		eval "$(conda shell.bash hook)"
-		conda activate ${toolDir}/dcm2bids
+		echo "${sharedDir}/dcm2bids_files/${study}_config.json"
 		
 		# convert to BIDS [more options: dcm2bids --help]
 			# -d: directory with dicom data
 			# -p: participant ID
-			# -s: session ID
+			# -s: session ID (none)
 			# -c: configuration file
 			# -o: output directory
-		if [[ ${sessions} == 'yes' ]]
-		then
-			dcm2bids -d ${tmpDir}/${NAME}/*/ -p ${NAME} -s 01 -c ${config} -o ${tmpDir}
-			subDir="sub-${NAME}/ses-01"
-			file_prefix="sub-${NAME}_ses-01"
-		else
-			dcm2bids -d ${tmpDir}/${NAME}/*/ -p ${NAME} -c ${config} -o ${tmpDir}
-			subDir="sub-${NAME}"
-			file_prefix="sub-${NAME}"
-		fi
-		
-		# deactivate conda environment
-		conda deactivate
-		
-		# if participant only has 1 run of pixar data, rename to run-01 (default is no run info)
-		if [ -f ${tmpDir}/${subDir}/func/${file_prefix}_task-pixar_bold.nii.gz ]
-		then
-			# remame files
-			mv ${tmpDir}/${subDir}/func/${file_prefix}_task-pixar_bold.nii.gz ${tmpDir}/${subDir}/func/${file_prefix}_task-pixar_run-01_bold.nii.gz
-			mv ${tmpDir}/${subDir}/func/${file_prefix}_task-pixar_bold.json ${tmpDir}/${subDir}/func/${file_prefix}_task-pixar_run-01_bold.json
-		fi
+		apptainer exec -B /RichardsonLab:/RichardsonLab,/${bidsDir}:/${bidsDir}		\
+		  ${singularityDir}/dcm2bids.simg											\
+		  dcm2bids																	\
+			-d ${tmpDir}/${raw_name}												\
+			-p sub-${bids_name}														\
+			-c ${sharedDir}/dcm2bids_files/${study}/sub-${bids_name}_config.json	\
+			-o ${tmpDir}
 		
 		# remove tmp conversion directories
-		rm -R ${tmpDir}/tmp_dcm2bids
-		rm -R ${tmpDir}/${NAME}
+		rm -r ${tmpDir}/tmp_dcm2bids
+		rm -r ${tmpDir}/${raw_name}
 		
-		# copy temporary folder to BIDS_data folder
-		cp -r ${tmpDir}/sub-${NAME} ${bidsDir}
-		
-		# remove the default diffusion data files
-		rm ${bidsDir}/${subDir}/dwi/*.nii.gz
-		rm ${bidsDir}/${subDir}/dwi/*.bv*
-		
-		# generate new diffusion files (more info: https://mrtrix.readthedocs.io/en/dev/reference/commands/dwiextract.html)
-		/EBC/local/MRtrix3_stable/mrtrix3/bin/dwiextract ${tmpDir}/${subDir}/dwi/${file_prefix}_dwi.nii.gz \
-														 -fslgrad ${tmpDir}/${subDir}/dwi/${file_prefix}_dwi.bvec ${tmpDir}/${subDir}/dwi/${file_prefix}_dwi.bval \
-														 -shells 0,500,1000,2000 ${bidsDir}/${subDir}/dwi/${file_prefix}_dwi.nii.gz \
-														 -export_grad_fsl ${bidsDir}/${subDir}/dwi/${file_prefix}_dwi.bvec ${bidsDir}/${subDir}/dwi/${file_prefix}_dwi.bval
-		
-		# give other users permissions to the generated folder
-		chmod -R a+rwx ${bidsDir}/sub-${NAME}
+		# copy temporary folder to BIDS folder
+		mv ${tmpDir}/sub-${bids_name} ${bidsDir}
 	
 	fi
 	
-done <$1
+done <$2
 
 # remove temporary data conversion directory
 rm -r ${tmpDir}
