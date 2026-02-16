fb90acf99342770698e83af18a432e565bca24a7
Melissa Thye <melissathye@gmail.com>
2026-02-16 15:45:46 +0000

    add updated percent signal change script

--- scripts/06.first_level/calc_psc.py
+++ scripts/06.first_level/calc_psc.py
@@ -2,31 +2,43 @@
 Calculate percent signal change using the outputs from the firstlevel model.
 
 This script will:
-(1) calculate and output the voxelwise mean signal value
+(1) calculate the voxelwise mean signal value of the scaled, filtered, artifact nan'd data
 (2) calculate percent signal change for requested conditions (from config file) using the following approach:
-PSC = (voxelwise beta map / voxelwise mean signal value) * 100
+PSC = (cope map * PP Height [peak to peak height] * 100)/ voxelwise mean
+
+The script assumes that cope file (rather than parameter estimate files are being used, 
+so the design.con file rather than the design.mat file is used to extract the PP Height.
+
+In order to analyse a single condition beta/parameter estimate, it is assumed that users will greater a 'contrast'
+that is weights the single condition by 1 (e.g., [1 0 0]), so a labelled contrast file is generated but no contrast is actually done.
 
 The outputs are saved in the *psc* folder for each run for each subject. 
 
 This script should be run prior to the extract stats script is the user wants to extract mean PSC values.
 
 """
-#from nipype.interfaces import fsl
+import sys
 from nipype.interfaces.fsl import MeanImage, BinaryMaths, ImageMaths
 import nipype.interfaces.io as nio
-#from nipype import Workflow, Node, MapNode, IdentityInterface, Function, DataSink, JoinNode, SelectFiles
 from nipype import Workflow, Node
+import nibabel as nib
+from nibabel import load
+import nilearn
+from nilearn import image
+from bids.layout import BIDSLayout
+import re
 import os
 import os.path as op
 import glob
 import numpy as np
 import pandas as pd
+from pandas.errors import EmptyDataError
 import argparse
 import glob
 import shutil
 
 # define calc psc workflow function
-def calc_psc_workflow(projDir, resultsDir, smoothDir, workDir, sub, ses, task, sub_runs, contrast_opts, splithalf_id, name='{}_task-{}_calcpsc'):
+def calc_psc_workflow(projDir, derivDir, resultsDir, workDir, sub, ses, task, sub_runs, contrast_opts, splithalf_id, hpf, filter_opt, TR, space_name, name='{}_task-{}_calcpsc'):
 
     # define subject output directory
     subDir = op.join(resultsDir, '{}'.format(sub))
@@ -43,71 +55,166 @@ def calc_psc_workflow(projDir, resultsDir, smoothDir, workDir, sub, ses, task, s
     # initialize workflow
     wf = Workflow(name=name.format(sub, task),
                   base_dir=workDir)
+    
+    # grab MNI mask
+    if ses != 'no': # if session was provided
+        # define path to preprocessed functional and mask data (subject derivatives func folder)
+        funcDir = op.join(derivDir, '{}'.format(sub), 'ses-{}'.format(ses), 'func')
+        mni_mask = glob.glob(op.join(funcDir, '{}_ses-{}_space-{}*_desc-brain_mask_allruns-BOLDmask.nii.gz'.format(sub, ses, space_name)))[0]
+        
+    else: # if session was 'no'
+        # define path to preprocessed functional and mask data (subject derivatives func folder)
+        funcDir = op.join(derivDir, '{}'.format(sub), 'func')
+        mni_mask = glob.glob(op.join(funcDir, '{}_space-{}*_desc-brain_mask_allruns-BOLDmask.nii.gz'.format(sub, space_name)))[0]
                   
     # for each run
     for r in sub_runs:
-        # create output directory for psc outputs
+        # define directories based on run and splithalf info
         if splithalf_id == 0:
             pscDir = op.join(subDir, 'psc', 'run{}'.format(r))
+            designDir = op.join(subDir, 'design', 'run{}'.format(r))
+            modelDir = op.join(subDir, 'model', 'run{}'.format(r))
+            preprocDir = op.join(subDir, 'preproc', 'run{}'.format(r))
+            
+            # grab rapidart outlier file
+            art_file = glob.glob(op.join(subDir, 'art_files', 'run{}'.format(r), '*out-vols.txt'))[0]
+            
+            # save intermediate file - optional output for data checking
+            denoised_file = op.join(pscDir, '{}_task-{}_run-{:03d}_denoised.nii.gz'.format(sub, task, r))
+            
+            # define output run mean file - optional output for data checking
+            mean_file = op.join(pscDir, '{}_task-{}_run-{:03d}_voxelwise_mean.nii.gz'.format(sub, task, r))
+            
         else:
-            pscDir = op.join(subDir, 'psc', 'run()_splithalf{}'.format(r, splithalf_id))
+            pscDir = op.join(subDir, 'psc', 'run{}_splithalf{}'.format(r, splithalf_id))
+            designDir = op.join(subDir, 'design', 'run{}_splithalf{}'.format(r, splithalf_id))
+            modelDir = op.join(subDir, 'model', 'run{}_splithalf{}'.format(r, splithalf_id))
+            preprocDir = op.join(subDir, 'preproc', 'run{}_splithalf{}'.format(r, splithalf_id))
+            
+            # grab rapidart outlier file
+            art_file = glob.glob(op.join(subDir, 'art_files', 'run{}_splithalf{}'.format(r, splithalf_id), '*out-vols.txt'))[0]
+            
+            # save intermediate file - optional output for data checking
+            denoised_file = op.join(pscDir, '{}_task-{}_run-{:03d}_splithalf-{:03d}_denoised.nii.gz'.format(sub, task, r, splithalf_id))
+            
+            # define output run mean file - optional output for data checking
+            mean_file = op.join(pscDir, '{}_task-{}_run-{:03d}_splithalf-{:03d}_voxelwise_mean.nii.gz'.format(sub, task, r, splithalf_id))
         
+        # grab design file (has PP heights)
+        design_file = op.join(designDir, 'run{}.con'.format(r))
+            
+        # make psc directory
         os.makedirs(pscDir, exist_ok=True)
         
-        # grab preproc file (depending on smoothing options in config file)
-        if smoothDir:
-            preprocDir = op.join(smoothDir, '{}'.format(sub), 'preproc', 'run{}'.format(r))
+        # grab scaled preproc file
+        scaled_file = glob.glob(op.join(preprocDir, '*_scaled.nii.gz'))[0]
+        
+        if not scaled_file:
+            raise FileNotFoundError('No scaled firstlevel outputs found for {}'.format(sub))
+        
+        print('Will denoise the following file to calculate voxelwise mean in run {}: {}'.format(r, scaled_file))
+        
+        # get number of volumes in scaled data (accounts for dropped/split data already)
+        nVols = (load(scaled_file).shape[3])
+        
+        print('Number of volumes in run {}: {}'.format(r, nVols))
+        
+        # generate vector of volume indices (where inclusion means to retain volume) to use for scrubbing
+        vol_indx = np.arange(nVols, dtype=np.int64)
+        
+        # read in art file, creating an empty dataframe if no outlier volumes (i.e., empty text file)
+        try:
+            outliers = pd.read_csv(art_file, header=None)[0].astype(int)
+        except EmptyDataError:
+            outliers = pd.DataFrame()
+        
+        print('ART identified motion spikes will be scrubbed from data')             
+        if np.shape(outliers)[0] != 0: # if there are outlier volumes
+            # remove excluded volumes from vec
+            vol_indx = np.delete(vol_indx, [outliers])
+            print('{} outlier volumes will be scrubbed in run {}'.format(len(outliers), r))
         else:
-            preprocDir = op.join(resultsDir, '{}'.format(sub), 'preproc', 'run{}'.format(r))
+            print('No outlier volumes in run {}'.format(r))
         
-        # default to assuming preproc file is smooth, but use nonsmoothed output if not
-        preproc_file = glob.glob(op.join(preprocDir, '*_smooth.nii.gz'))
-        if not preproc_file:
-            preproc_file = glob.glob(op.join(preprocDir, '*_bold.nii.gz'))
-            
-        print('Will use the following file to calculate voxelwise mean in run {}: {}'.format(r, preproc_file))
+        # convert filter from seconds to Hz
+        hpf_hz = 1/hpf
+        print('Will apply a {} filter using a high pass filter cutoff of {}Hz for run {}.'.format(filter_opt, hpf_hz, r))
         
-        # define output run mean file
-        mean_file = op.join(pscDir, 'voxelwise_mean.nii.gz')
+        # define kwargs input to signal.clean function
+        if filter_opt == 'butterworth':
+            kwargs_opts={'clean__sample_mask':vol_indx, 
+                         'clean__butterworth__t_r':TR,
+                         'clean__butterworth__high_pass':hpf_hz}
+        elif filter_opt == 'cosine':
+            kwargs_opts={'clean__sample_mask':vol_indx, 
+                         'clean__cosine__t_r':TR,
+                         'clean__cosine__high_pass':hpf_hz}
+        else:
+            kwargs_opts={'clean__sample_mask':vol_indx,
+                         'clean__t_r':TR}
+        
+        # filter and remove artifact timepoints in scaled data file
+        denoised_data = image.clean_img(scaled_file, mask_img=mni_mask, detrend=False, standardize=False, **kwargs_opts)
+        
+        # save denoised data
+        nib.save(denoised_data, denoised_file)
         
         # step 1: calculate voxelwise mean across this run
-        meanfunc = Node(MeanImage(), name='meanfunc_run{}'.format(r))
+        meanfunc = Node(MeanImage(), name='meanfunc_run{}_splithalf{}'.format(r, splithalf_id))
         meanfunc.inputs.dimension = 'T' # returns the mean across time
-        meanfunc.inputs.in_file = preproc_file[0]
-        meanfunc.inputs.out_file = mean_file
+        meanfunc.inputs.in_file = denoised_file
+        #meanfunc.inputs.out_file = mean_file # can return this file as a data checking step
         
         # add the meanfunc node to the workflow
         wf.add_nodes([meanfunc])
-
-        # loop over beta maps
-        for b in contrast_opts:
-            print('Calculating percent signal change for {} condition'.format(b))
+        meanfunc.run()
+        
+        # remove intermediate file
+        os.remove(denoised_file)
+        
+        # loop over cope maps
+        for cope in contrast_opts:
+            print('Calculating percent signal change for {} condition'.format(cope))
             
-            # grab copes file corresponding to beta map
-            beta_file = glob.glob(op.join(subDir, 'model', 'run{}'.format(r), 'con*_{}_cope.nii.gz'.format(b)))[0]
+            # grab copes file corresponding to cope map
+            cope_file = glob.glob(op.join(modelDir, 'con*_{}_cope.nii.gz'.format(cope)))[0]
             
-            print('Using {} cope file for calculation'.format(beta_file))
+            # extract contrast number
+            con_num = int(re.search(r'con_(\d+)', cope_file).group(1))
+            
+            print('Using cope file: {}'.format(cope_file))
+            
+            # read in ppheights from design file
+            with open(design_file, 'r') as d:
+                for line in d:
+                    if line.startswith('/PPheights'):
+                        ppheights = [float(x) for x in line.split()[1:]]
+                        
+            # use contrast number to select correct pp height
+            pp_value = ppheights[con_num - 1] # subtract 1 because of 0 indexing in python
+            
+            print('Using contrast peak-to-peak height {} in the PSC calculation: {}'.format(con_num, pp_value))
             
             # define psc output files
-            div_file = op.join(pscDir, '{}_divided.nii.gz'.format(b))
-            psc_file = op.join(pscDir, '{}_psc.nii.gz'.format(b))
-
-            # step 2: divide beta map by the voxelwise mean_file
-            dividefunc = Node(BinaryMaths(), name='dividefunc_run{}_{}'.format(r,b))
-            dividefunc.inputs.operation = 'div' # divide
-            dividefunc.inputs.in_file = beta_file
-            #dividefunc.inputs.out_file = div_file # can return this file as a data checking step
+            mul_file = op.join(pscDir, '{}_multiplied.nii.gz'.format(cope))
+            psc_file = op.join(pscDir, '{}_psc.nii.gz'.format(cope))
             
-            # step 3: multiply output maps by 100 to get percent
-            multiplyfunc = Node(ImageMaths(), name='multiplyfunc_run{}_{}'.format(r,b))
-            multiplyfunc.inputs.args = '-mul 100'
-            multiplyfunc.inputs.out_file = psc_file
+            # step 2: multiply cope file by PP height * 100
+            pp_scale_factor = pp_value * 100
+            multiplycope = Node(ImageMaths(), name='multiplycope_run{}_splithalf{}_{}'.format(r, splithalf_id, cope))
+            multiplycope.inputs.in_file = cope_file
+            multiplycope.inputs.args = '-mul {}'.format(pp_scale_factor)
+            #multiplycope.inputs.out_file = mul_file # can return this file as a data checking step
+
+            # step 3: divide cope map by the voxelwise mean_file
+            dividecope = Node(BinaryMaths(), name='dividecope_run{}_splithalf{}_{}'.format(r, splithalf_id, cope))
+            dividecope.inputs.operation = 'div' # divide
+            dividecope.inputs.out_file = psc_file
             
             # add nodes to workflow and connect inputs
-            wf.add_nodes([dividefunc, multiplyfunc])
-            wf.connect(meanfunc, 'out_file', dividefunc, 'operand_file')
-            wf.connect(dividefunc, 'out_file', multiplyfunc, 'in_file')
-                        
+            wf.add_nodes([multiplycope, dividecope])
+            wf.connect(multiplycope, 'out_file', dividecope, 'in_file')
+            wf.connect(meanfunc, 'out_file', dividecope, 'operand_file')
             
     return wf
 
@@ -136,6 +243,9 @@ def argparser():
 
 # define main function that parses the config file and runs the functions defined above
 def main(argv=None):
+    # don't buffer messages
+    sys.stdout = open(sys.stdout.fileno(), mode='w', buffering=1)
+    
     # call argparser function that defines command line inputs
     parser = argparser()
     args = parser.parse_args(argv)   
@@ -150,12 +260,16 @@ def main(argv=None):
     
     # read in configuration file and parse inputs
     config_file=pd.read_csv(args.config, sep='\t', header=None, index_col=0).replace({np.nan: None})
+    bidsDir=config_file.loc['bidsDir',1]
+    derivDir=config_file.loc['derivDir',1]
     resultsDir=config_file.loc['resultsDir',1]
-    smoothDir=config_file.loc['smoothDir',1]
     task=config_file.loc['task',1]
     ses=config_file.loc['sessions',1]
     contrast_opts=config_file.loc['contrast',1].replace(' ','').split(',')
     splithalf=config_file.loc['splithalf',1]
+    hpf=int(config_file.loc['hpf',1])
+    filter_opt=config_file.loc['filter',1]
+    space=config_file.loc['space',1]
     
     # define working directory
     workDir = op.join(resultsDir, 'processing')
@@ -168,6 +282,14 @@ def main(argv=None):
     else:
         splithalves = [0]
     
+    # define space name
+    if space == 'MNI':
+        space_name = 'MNI152NLin2009cAsym'
+        print('Pipeline will be run using outputs in {} space'.format(space_name))
+    if space == 'native':
+        space_name = 'T1w'
+        print('Pipeline will be run using outputs in {} space'.format(space_name))
+        
     # identify analysis README file
     readme_file=op.join(resultsDir, 'README.txt')
     
@@ -176,6 +298,15 @@ def main(argv=None):
         file_1.write('\n')
         file_1.write('Percent signal change was calculated using the calc_psc.py script and options specified in the config file: {} \n'.format(args.config))
 
+    # get layout of BIDS directory
+    # this is necessary because the pipeline reads the functional json files that have TR info
+    # the derivDir (where fMRIPrep outputs are) doesn't have json files with this information, so getting the layout of that directory will result in an error
+    layout = BIDSLayout(bidsDir)
+    
+    # extract TR info from bidsDir bold json files (assumes TR is same across runs)
+    epi = layout.get(suffix='bold', task=task, return_type='file')[0] # take first file
+    TR = layout.get_metadata(epi)['RepetitionTime'] # extract TR field  
+    
     # for each subject in the list of subjects
     for index, sub in enumerate(args.subjects):
         for splithalf_id in splithalves:
@@ -192,7 +323,7 @@ def main(argv=None):
                 sub_runs=list(map(int, sub_runs)) # convert to integers
                   
             # create calc psc workflow with the inputs defined above
-            wf = calc_psc_workflow(args.projDir, resultsDir, smoothDir, workDir, sub, ses, task, sub_runs, contrast_opts, splithalf_id)
+            wf = calc_psc_workflow(args.projDir, derivDir, resultsDir, workDir, sub, ses, task, sub_runs, contrast_opts, splithalf_id, hpf, filter_opt, TR, space_name)
        
             # configure workflow options
             wf.config['execution'] = {'crashfile_format': 'txt',
